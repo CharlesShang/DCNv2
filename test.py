617458f594371df65c6e4d227b634cfc9eaca0db
CharlesShang <shangchong90@gmail.com>
2018-12-10 10:42:59 +0800

    test examples

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ from torch.autograd import gradcheck
 
 from dcn_v2 import DCNv2
 from dcn_v2_func import DCNv2Function
+from dcn_v2 import DCNv2Pooling
+from dcn_v2_func import DCNv2PoolingFunction
 
 deformable_groups = 1
 N, inC, inH, inW = 2, 2, 4, 4
@@ -62,7 +64,7 @@ def check_zero_offset():
     else:
         print('Zero offset failed')
 
-def check_gradient_double():
+def check_gradient_dconv_double():
 
     input = torch.randn(N, inC, inH, inW, dtype=torch.float64).cuda()
     input.requires_grad = True
@@ -87,7 +89,7 @@ def check_gradient_double():
 
     print(gradcheck(func, (input, offset, mask, weight, bias), eps=1e-6, atol=1e-5, rtol=1e-3))
 
-def check_gradient():
+def check_gradient_dconv():
 
     input = torch.randn(N, inC, inH, inW).cuda()
     input.requires_grad = True
@@ -143,6 +145,16 @@ def check_pooling_zero_offset():
     s = ', '.join(['%f' % dout[i, :, :, :].mean().item() for i in range(rois.shape[0])])
     print(s)
 
+# def check_gradient_dpooling():
+#     input = torch.randn(2, 32, 64, 64).cuda()
+#     batch_inds = torch.randint(2, (20, 1)).cuda().float()
+#     x = torch.randint(256, (20, 1)).cuda().float()
+#     y = torch.randint(256, (20, 1)).cuda().float()
+#     w = torch.randint(64, (20, 1)).cuda().float()
+#     h = torch.randint(64, (20, 1)).cuda().float()
+#     rois = torch.cat((batch_inds, x, y, x + w, y + h), dim=1)
+#     offset = torch.randn(20, 2, 7, 7).cuda()
+
 def example_dconv():
     from dcn_v2 import DCN
     input = torch.randn(2, 64, 128, 128).cuda()
@@ -165,6 +177,8 @@ def example_dpooling():
     h = torch.randint(64, (20, 1)).cuda().float()
     rois = torch.cat((batch_inds, x, y, x + w, y + h), dim=1)
     offset = torch.randn(20, 2, 7, 7).cuda()
+    input.requires_grad = True
+    offset.requires_grad = True
 
     # normal roi_align
     pooling = DCNv2Pooling(spatial_scale=1.0 / 4,
@@ -187,9 +201,19 @@ def example_dpooling():
     print(out.shape)
     print(dout.shape)
 
+    target_out = out.new(*out.size())
+    target_out.data.uniform_(-0.01, 0.01)
+    target_dout = dout.new(*dout.size())
+    target_dout.data.uniform_(-0.01, 0.01)
+    e = (target_out - out).mean()
+    e.backward()
+    e = (target_dout - dout).mean()
+    e.backward()
+
 def example_mdpooling():
     from dcn_v2 import DCNPooling
     input = torch.randn(2, 32, 64, 64).cuda()
+    input.requires_grad = True
     batch_inds = torch.randint(2, (20, 1)).cuda().float()
     x = torch.randint(256, (20, 1)).cuda().float()
     y = torch.randint(256, (20, 1)).cuda().float()
@@ -206,6 +230,10 @@ def example_mdpooling():
                          trans_std=0.1).cuda()
 
     dout = dpooling(input, rois)
+    target = dout.new(*dout.size())
+    target.data.uniform_(-0.1, 0.1)
+    error = (target - dout).mean()
+    error.backward()
     print(dout.shape)
 
 if __name__ == '__main__':
